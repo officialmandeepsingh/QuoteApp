ca2b39f316a449ac008454d543ba9054cdc004e4
Mandeep Singh <officialmandeepsp@gmail.com>
2023-05-12 22:19:02 +0530

    Clipboard Setup

---
+++ app/src/main/java/com/mandeepspurwa/quotesapp/util/ClipBoard.kt
@@ -0,0 +1,12 @@
+package com.mandeepspurwa.quotesapp.util
+
+/**
+ * App Name: Quotes App
+ * Package Name: com.mandeepspurwa.quotesapp.util
+ * Author : Mandeep Singh
+ * Email Id: officialmandeepsp@gmail.com
+ * Date: Fri 12 May, 2023
+ *
+ **/
+object ClipBoard {
+}
